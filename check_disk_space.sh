477abf43410fd2dc80ac9972b902d043592ac529
Nithe14 <imnithe@gmail.com>
2022-03-19 23:56:28 +0100

    Add main script

---
+++ check_disk_space.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+MOUNT_POINT="/System/Volumes/Data"	#Your mount point to check
+WARN_MESSEGE_VALUE=50 			#percentage
+CRITICAL_MESSEGE_VALUE=80 		#percentage
+
+check_messege=`df $MOUNT_POINT` 
+set -- $check_messege
+used_disk_proc=${15}
+used_disk=`echo $used_disk_proc | sed 's/\%//'`
+
+if [ $used_disk -ge $CRITICAL_MESSEGE_VALUE ]
+then
+        echo -e "\033[41mYour disk is over $CRITICAL_MESSEGE_VALUE full! ($MOUNT_POINT) - $used_disk_proc"        #Red backgroud critical messege
+elif [ $used_disk -ge $WARN_MESSEGE_VALUE ]
+then
+	echo -e "\033[0;33mYour disk is over $WARN_MESSEGE_VALUE full! ($MOUNT_POINT) - $used_disk_proc"	#Yellow warn messege
+else
+	echo "Your disk is $used_disk_proc full! ($MOUNT_POINT)"
+fi
